aab539e1a14226be34bebdd5396026cf22cf4a46
reham <rehamnaguib1@gmail.com>
2018-12-07 18:35:52 +0200

    interfaces

--- JDBC/src/eg/edu/alexu/csd/oop/jdbc/cs17/SQLDriver.java
+++ JDBC/src/eg/edu/alexu/csd/oop/jdbc/cs17/SQLDriver.java
@@ -1,27 +1,29 @@
 package eg.edu.alexu.csd.oop.jdbc.cs17;
 
 import java.io.File;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.logging.Logger;
 
-import eg.edu.alexu.csd.oop.jdbc.Connection;
-import eg.edu.alexu.csd.oop.jdbc.Driver;
 
 public class SQLDriver implements Driver {
 
 	@Override
 	public boolean acceptsURL(String url) throws SQLException {
-       if(url.equals("jdbc:xmldb://localhost:")) { 
-    	   return true;
-       }
-       else {
-    	   return false;
-       }
+		 if(url.equals("jdbc:xmldb://localhost:")) { 
+	    	   return true;
+	       }
+	       else {
+	    	   return false;
+	       }
 	}
 
 	@Override
-	public Connection connect(String url, Properties info) throws SQLException {
-		// TODO Auto-generated method stub
+	public java.sql.Connection connect(String url, Properties info) throws SQLException {
 		if(!acceptsURL(url)) {
 			return null;
 		} else {
@@ -32,9 +34,32 @@ public class SQLDriver implements Driver {
 	}
 
 	@Override
+	public int getMajorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMinorVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
 	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
+	public boolean jdbcCompliant() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
